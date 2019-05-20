c253a7ca7112dbec1b0af6eb679c159a6a12fa39
Marnix Harderwijk <marnix.harderwijk@rocketscience.io>
2019-05-20 21:29:45 +0200

    add analytics

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -2,7 +2,7 @@ export const init = () => {
   // Initialize the command queue in case analytics.js hasn't loaded yet.
   window.ga = window.ga || ((...args) => (ga.q = ga.q || []).push(args));
 
-  ga('create', 'UA-9750732-3', 'auto');
+  ga('create', 'XXXXX', 'auto');
   ga('set', 'transport', 'beacon');
   ga('send', 'pageview');
 };
