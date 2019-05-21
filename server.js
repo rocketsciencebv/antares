93a37d5cb060a9ce8cb7ad0d7b1ac2f43cb6d5a8
Marnix Harderwijk <marnix.harderwijk@rocketscience.io>
2019-05-21 09:01:13 +0200

    remove certificate verification

--- server.js
+++ server.js
@@ -13,10 +13,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname + '/dist'));
 });
 
-app.get('/.well-known/acme-challenge/:content', function(req, res) {
-  res.send('ALV5cdt8zzb2Eyq-KoD-08mcLB9QsF-tEsQ_5fEP6QQ.HyQ2v9gIKchGp_XMb2QTqN2gBXl7ghE0kk1M-F_o_5w');
-});
-
 app.redirect('*', '/', 301);
 
 app.listen(port);
